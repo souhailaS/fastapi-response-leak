8daebbfbbff7e9835037525e310c484ce7d11c70
souhailaS <serbout.souhaila@gmail.com>
2025-11-29 19:55:23 +0100

    Add CORS support

--- vulnerable_middleware.py
+++ vulnerable_middleware.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Dict, Any
 import asyncio
 import uvicorn
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.middleware("http")
